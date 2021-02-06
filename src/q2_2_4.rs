5b6aa47cbe5bd4d8d22d9c6caaaea22e9d70187e
snicmakino <snicmakino@gmail.com>
2021-02-07 06:47:22 +0900

    2_2_4 のエッジケースがエラーとなっていたため修正

--- src/q2_2_4.rs
+++ src/q2_2_4.rs
@@ -5,8 +5,8 @@ mod q2_2_4 {
         let mut ans = 0;
         let mut min = x[0];
 
-        for i in 0..n {
-            if min + r < x[i] {
+        for i in 1..n {
+            if min + r <= x[i] {
                 ans += 1;
                 min = x[i];
             }
@@ -25,5 +25,19 @@ mod tests {
         let r = 10;
         let x = vec![1, 7, 15, 20, 30, 50];
         assert_eq!(solve(n, r, x), 3);
+    }
+    #[test]
+    fn test2() {
+        let n = 3;
+        let r = 0;
+        let x = vec![10, 20, 20];
+        assert_eq!(solve(n, r, x), 2);
+    }
+    #[test]
+    fn test3() {
+        let n = 7;
+        let r = 10;
+        let x = vec![70, 30, 1, 7, 15, 20, 50];
+        assert_eq!(solve(n, r, x), 4);
     }
 }
