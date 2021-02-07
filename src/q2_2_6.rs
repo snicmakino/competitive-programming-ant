de8ad3ffffadb2f2d5150dbfb0f7ca0332c0e90a
snicmakino <snicmakino@gmail.com>
2021-02-08 02:36:40 +0900

    q2_2_6 リファクタリング

--- src/q2_2_6.rs
+++ src/q2_2_6.rs
@@ -2,43 +2,41 @@ mod q2_2_6 {
     use std::collections::HashMap;
 
     pub fn solve(n: usize, wv: Vec<(i32, i32)>, maxw: i32) -> i32 {
-        let mut dfs = Dfs::new(n, wv, maxw);
-        dfs.dfs(0, 0, 0)
+        let mut dfs = Dfs::new(n, wv);
+        dfs.dfs(0, maxw)
     }
 
     struct Dfs {
         n: usize,
         wv: Vec<(i32, i32)>,
-        maxw: i32,
         memo: HashMap<(usize, i32), i32>,
     }
 
     impl Dfs {
-        pub fn new(n: usize, wv: Vec<(i32, i32)>, maxw: i32) -> Dfs {
+        pub fn new(n: usize, wv: Vec<(i32, i32)>) -> Dfs {
             Dfs {
                 n,
                 wv,
-                maxw,
                 memo: HashMap::new(),
             }
         }
 
-        pub fn dfs(&mut self, i: usize, cw: i32, cv: i32) -> i32 {
-            if self.memo.contains_key(&(i, cw)) {
-                return *self.memo.get(&(i, cw)).unwrap();
+        pub fn dfs(&mut self, i: usize, w: i32) -> i32 {
+            if self.memo.contains_key(&(i, w)) {
+                return *self.memo.get(&(i, w)).unwrap();
             }
 
             if i == self.n {
-                return cv;
+                return 0;
             }
-            let tw = cw + self.wv[i].0;
-            let tv = cv + self.wv[i].1;
-            if self.maxw < tw {
-                return cv;
+            let cw = self.wv[i].0;
+            let cv = self.wv[i].1;
+            if w < cw {
+                return 0;
             }
 
-            let max = std::cmp::max(self.dfs(i + 1, cw, cv), self.dfs(i + 1, tw, tv));
-            self.memo.insert((i, cw), max);
+            let max = std::cmp::max(self.dfs(i + 1, w), self.dfs(i + 1, w - cw) + cv);
+            self.memo.insert((i, w), max);
             max
         }
     }
